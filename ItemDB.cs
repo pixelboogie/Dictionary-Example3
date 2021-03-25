f78339092348d37bfefcee209687e96466021312
pixelboogie <41452340+pixelboogie@users.noreply.github.com>
2021-03-24 18:51:06 -0700

    Add files via upload

---
+++ ItemDB.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemDB : MonoBehaviour
+{
+
+    public Dictionary<string, int> people = new Dictionary<string, int>();
+
+    private void Start()
+    {
+        people.Add("John", 11);
+        people.Add("James", 22);
+        people.Add("Rachel", 33);
+        people.Add("Yin", 44);
+
+        int rachelsAge = people["Rachel"];
+
+        Debug.Log("age: " + rachelsAge);
+
+    }
+
+}
+
+
